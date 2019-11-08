9a6999a10275fdd02c1eb90dbbc91de2946d6123
Caitlin <CM232388@falmouth.ac.uk>
2019-11-08 14:16:26 +0000

    * Removed green_reduction_amount variable
    * Changed variable from _make_less_blue_green to _make_less_blue
    
    * Added more comments explaining code
    
    * Refactored code to conform to PEP8 standards

--- Colour Blindness/ColourBlindness.py
+++ Colour Blindness/ColourBlindness.py
@@ -1,23 +1,26 @@
 import pygame
 
-# Variables
+
+# Variables for pop up window
 WINDOW_WIDTH = 480
 WINDOW_HEIGHT = 640
 WHITE = (255, 255, 255)
+
+# Variables for base image of fireworks
 BASE_IMAGE = "fireworks"
 BASE_IMAGE_EXTENSION = ".png"
 ORIGIN = (0, 0)
+
 # Variables for colour reduction
 REDUCTION_AMOUNT = 100
-RED_REDUCTION_AMOUNT = 175
-GREEN_REDUCTION_AMOUNT = 100
-BLUE_REDUCTION_AMOUNT = 100
+RED_REDUCTION_AMOUNT = 200
+BLUE_REDUCTION_AMOUNT = 125
 
+# Dictionary variables and dictionary
 LESS_RED = 1
-LESS_BLUE_GREEN = 2
+LESS_BLUE = 2
 GREYSCALE = 3
-# could this be a dictionary of different effects?
-EFFECT = {LESS_RED: "_less_red", LESS_BLUE_GREEN: "_less_blue_green", GREYSCALE: "_total_colour_blind"}
+EFFECT = {LESS_RED: "_less_red", LESS_BLUE: "_less_blue", GREYSCALE: "_total_colour_blind"}
 
 # Initialising PyGame
 pygame.init()
@@ -28,20 +31,24 @@ original_image = pygame.image.load(BASE_IMAGE + BASE_IMAGE_EXTENSION).convert()
 
 
 def main():
+    """
+    Main function
+    """
     main_window.fill(WHITE)
     main_window.blit(original_image, ORIGIN)
 
-    effect = make_less_red(original_image, RED_REDUCTION_AMOUNT)
     # Saving the make_less_red image under a new name
+    effect = make_less_red(original_image, RED_REDUCTION_AMOUNT)
     pygame.image.save(original_image, BASE_IMAGE + EFFECT[effect] + BASE_IMAGE_EXTENSION)
 
-    effect = make_less_blue_green(original_image, GREEN_REDUCTION_AMOUNT, BLUE_REDUCTION_AMOUNT)
     # Saving the make_less_blue_green image under a new name
+    effect = make_less_blue(original_image, BLUE_REDUCTION_AMOUNT)
     pygame.image.save(original_image, BASE_IMAGE + EFFECT[effect] + BASE_IMAGE_EXTENSION)
 
-    effect = make_greyscale(original_image, REDUCTION_AMOUNT)
     # Saving the greyscale image under a new name
+    effect = make_greyscale(original_image, REDUCTION_AMOUNT)
     pygame.image.save(original_image, BASE_IMAGE + EFFECT[effect] + BASE_IMAGE_EXTENSION)
+
     main_window.blit(original_image, ORIGIN)
 
     pygame.display.update()
@@ -56,7 +63,10 @@ def main():
 
 
 def make_less_red(surface, red_reduction_amount):
-    """Function to loop through the image pixels to remove red colouring"""
+    """
+    This function is to replicate an image of the colour blindness protanopia.
+    The function will loop through the image pixels to remove red colouring.
+    """
 
     for x in range(0, surface.get_width()):
         for y in range(0, surface.get_height()):
@@ -72,29 +82,31 @@ def make_less_red(surface, red_reduction_amount):
     return LESS_RED
 
 
-def make_less_blue_green(surface, green_reduction_amount, blue_reduction_amount):
-    """Function to loop through the image pixels to remove blue and green colouring"""
+def make_less_blue(surface, blue_reduction_amount):
+    """
+    This function is to replicate an image of colour blindness tritanopia.
+    The function will loop through the image pixels to remove blue colouring.
+    """
 
     for x in range(0, surface.get_width()):
         for y in range(0, surface.get_height()):
             pixel_colour = surface.get_at((x, y))
-            new_green = pixel_colour.g - green_reduction_amount
             new_blue = pixel_colour.b - blue_reduction_amount
-
-            if new_green < 0:
-                new_green = 0
 
             if new_blue < 0:
                 new_blue = 0
 
-            pixel_colour = (pixel_colour.r, new_green, new_blue)
+            pixel_colour = (pixel_colour.r, pixel_colour.g, new_blue)
             surface.set_at((x, y), pixel_colour)
 
-    return LESS_BLUE_GREEN
+    return LESS_BLUE
 
 
 def make_greyscale(surface, reduction_amount):
-    """Function to loop through the image pixels to remove colour"""
+    """
+    This function is to replicate an image of colour blindness monochromacy.
+    Function to loop through the image pixels to remove colour.
+    """
 
     for x in range(0, surface.get_width()):
         for y in range(0, surface.get_height()):
